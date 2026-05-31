0fac0e6090250ba00915c80d67fe69aed82af0ec
bjmhe <hi@bjmhe.me>
2026-06-01 01:36:25 +0800

    Add 'fund' command to CLI in index.ts to fetch and save FUNDING.yml from GitHub repository, replacing previous lint command.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 #!/usr/bin/env node
 import cac from "cac";
+import fs from "node:fs";
 import { version } from "../package.json";
 
 const cli = cac();
 
-cli.command("lint [...files]", "Lint files")
-  .action((files, options) => {
-    console.log(files, options);
+cli.command("fund", "Fetch FUNDING.yml from @bjmhe")
+  .action(async () => {
+    // 拉取 https://raw.githubusercontent.com/bjmhe/bjmhe/refs/heads/master/.github/FUNDING.yml 到本地的 .github/FUNDING.yml
+    const fundingUrl = "https://raw.githubusercontent.com/bjmhe/bjmhe/refs/heads/master/.github/FUNDING.yml";
+    const funding = await fetch(fundingUrl);
+    const fundingContent = await funding.text();
+    fs.writeFileSync(".github/FUNDING.yml", fundingContent);
   })
 
 cli.help();
